f7038462851b8bdf2681d209968712e502f7e355
Erickson <ericksonkinyua681@gmail.com>
2026-06-28 11:49:16 +0300

    2try

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -10,7 +10,7 @@ router = APIRouter(tags=["Authentication"])
 
 @router.post("/login", response_model=schema.Token)#user_credentials: OAuth2PasswordRequestForm = Depends()
 def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):#username: str = Header(...), password: str = Header(...), db: Session = Depends(get_db)):
-    user = db.query(models.User).filter(models.User.email == username).first()
+    user = db.query(models.User).filter(models.User.email == user_credentials.username).first()
 
     user = db.query(models.User).filter(models.User.email == user_credentials.username).first()
 
