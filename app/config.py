d6e7d304f62cb56b05b7cb294c66d96059f92fdf
Erickson <ericksonkinyua681@gmail.com>
2026-06-28 10:13:24 +0300

    Swittch t database_url for production providr by neon db

--- app/config.py
+++ app/config.py
@@ -1,12 +1,13 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    database_password: str
-    database_username: str
+    database_url:str
+    # database_password: str
+    # database_username: str
     secret_key: str
-    database_hostname: str
-    database_port: str
-    database_name: str
+    # database_hostname: str
+    # database_port: str
+    # database_name: str
     algorithm : str
     access_token_expire_time: str
     class Config:
--- app/database.py
+++ app/database.py
@@ -22,7 +22,9 @@ from .config import settings
 # if not all([user, host, port, name]):
 #     raise EnvironmentError("Missing database environment variables in .env file!")
 # # Construct the URL
-SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
+#SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
+# Simply use the URL directly
+SQLALCHEMY_DATABASE_URL = settings.database_url
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
