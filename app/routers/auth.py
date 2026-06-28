315060e37833ece73cdbddea262b9d55d9a2390b
Erickson <ericksonkinyua681@gmail.com>
2026-06-28 11:59:53 +0300

    3try

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -8,10 +8,9 @@ from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 
 router = APIRouter(tags=["Authentication"])
 
-@router.post("/login", response_model=schema.Token)#user_credentials: OAuth2PasswordRequestForm = Depends()
-def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):#username: str = Header(...), password: str = Header(...), db: Session = Depends(get_db)):
-    user = db.query(models.User).filter(models.User.email == user_credentials.username).first()
-
+@router.post("/login", response_model=schema.Token)
+def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    # OAuth2PasswordRequestForm gives you .username and .password
     user = db.query(models.User).filter(models.User.email == user_credentials.username).first()
 
     if not user:
@@ -22,7 +21,21 @@ def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session =
     
     access_token = auth2.create_access_token(data={"user_id": user.id})
     return {"access_token": access_token, "token_type": "bearer"}
-    # if not user:
+# @router.post("/login", response_model=schema.Token)#user_credentials: OAuth2PasswordRequestForm = Depends()
+# def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):#username: str = Header(...), password: str = Header(...), db: Session = Depends(get_db)):
+#     user = db.query(models.User).filter(models.User.email == user_credentials.username).first()
+
+#     user = db.query(models.User).filter(models.User.email == user_credentials.username).first()
+
+#     if not user:
+#         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Invalid Credentials")
+    
+#     if not utils.verify(user_credentials.password, user.password):
+#         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Invalid Credentials")
+    
+#     access_token = auth2.create_access_token(data={"user_id": user.id})
+#     return {"access_token": access_token, "token_type": "bearer"}
+#     # if not user:
     #     raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Invalid Credentials")
     
     # if not utils.verify(password, user.password):
