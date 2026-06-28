bb3e5d0dc2550f728097d05c95dfd4ea4b0df332
Erickson <ericksonkinyua681@gmail.com>
2026-06-28 11:47:50 +0300

    try

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -9,17 +9,27 @@ from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 router = APIRouter(tags=["Authentication"])
 
 @router.post("/login", response_model=schema.Token)#user_credentials: OAuth2PasswordRequestForm = Depends()
-def login(username: str = Header(...), password: str = Header(...), db: Session = Depends(get_db)):
+def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):#username: str = Header(...), password: str = Header(...), db: Session = Depends(get_db)):
     user = db.query(models.User).filter(models.User.email == username).first()
+
+    user = db.query(models.User).filter(models.User.email == user_credentials.username).first()
 
     if not user:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Invalid Credentials")
     
-    if not utils.verify(password, user.password):
+    if not utils.verify(user_credentials.password, user.password):
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Invalid Credentials")
-    
-    #create a token and return it
     
     access_token = auth2.create_access_token(data={"user_id": user.id})
     return {"access_token": access_token, "token_type": "bearer"}
+    # if not user:
+    #     raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Invalid Credentials")
+    
+    # if not utils.verify(password, user.password):
+    #     raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Invalid Credentials")
+    
+    # #create a token and return it
+    
+    # access_token = auth2.create_access_token(data={"user_id": user.id})
+    # return {"access_token": access_token, "token_type": "bearer"}
 
